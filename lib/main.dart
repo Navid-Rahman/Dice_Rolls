adb70cc899efd8bf13a884bccd0e2e79827a09a0
navid <navidrahman92@gmail.com>
2023-05-16 22:38:14 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Left Button got pressed');
+              },
+              child: const Image(
+                image: AssetImage('images/dice2.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Right Button got pressed');
+              },
+              child: const Image(
+                image: AssetImage('images/dice3.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
