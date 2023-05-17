b73095984bc8252e5ea1e9bf83d560d238ebc241
navid <navidrahman92@gmail.com>
2023-05-17 13:37:08 +0600

    Added chageDiceFace() function to clear out the codes more.

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 2;
   int rightDiceNumber = 1;
 
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -36,9 +43,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+                changeDiceFace();
               },
               child: Image(
                 image: AssetImage('images/dice$leftDiceNumber.png'),
@@ -48,9 +53,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+                changeDiceFace();
               },
               child: Image(
                 image: AssetImage('images/dice$rightDiceNumber.png'),
